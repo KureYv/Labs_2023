eb57bcf9c959b58da0fd9ad55f8049edb39976f6
Matthew <matthewlee031205@gmail.com>
2023-09-13 10:34:11 -0400

    Added some comments to lab 1

--- lab_1/Calculator.java
+++ lab_1/Calculator.java
@@ -1,8 +1,8 @@
-public class Calculator{
-    public double add(double x, double y){
-        return x+y;
+public class Calculator{ //Calculator class for calculating
+    public double add(double x, double y){ //Addition method
+        return x+y; //returns the addition x+y
     }
-    public double multiply(double x, double y){
-        return x*y;
+    public double multiply(double x, double y){ //Multiplication method
+        return x*y; //returns the multiplication x*y
     }
 }
--- lab_1/Driver.java
+++ lab_1/Driver.java
@@ -1,9 +1,9 @@
-public class Driver {
-    public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        double x = 5.0;
+public class Driver { //Driver class
+    public static void main(String[] args) { //Main method
+        Calculator calculator = new Calculator(); //Creating a new calculator object
+        double x = 5.0; //Declaring two floats x and y with different values
         double y = 6.0  ;
-        System.out.printf("%s + %s = "+calculator.add(x, y), x, y);
-        System.out.printf("\n%s * %s = "+calculator.multiply(x, y), x, y);
+        System.out.printf("%.1f + %.1f = %.1f", x, y, calculator.add(x, y)); //Printing the result with printf for addition formatting it to 1 dp 
+        System.out.printf("\n%.1f * %.1f = %.1f", x, y, calculator.multiply(x, y)); //Printing the result with printf for multiplication formatting it to 1 dp
     }
 }
